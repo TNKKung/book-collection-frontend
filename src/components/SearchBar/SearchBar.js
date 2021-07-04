f233500cf4cfe5d2b102154ee89b7f2428b15c6e
tomkungzer <kungzerkung@gmail.com>
2021-07-04 23:49:48 +0700

    update home page

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -2,22 +2,20 @@ import React from "react";
 import iconSearch from "./component/search.png";
 export default function SearchBar() {
   return (
-    <div>
-      <div className="flex justify-center items-center">
-        <div className="relative mt-10">
-          <div className="absolute mt-5 ml-3">
-            <img src={iconSearch} width="20" alt="IMG"></img>
-          </div>
-          <input
-            type="text"
-            className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
-            placeholder="Search anything..."
-          />
-          <div className="absolute top-2 right-2">
-            <button className="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-700">
-              Search
-            </button>
-          </div>
+    <div className="flex justify-center items-center">
+      <div className="relative mt-10">
+        <div className="absolute mt-5 ml-3">
+          <img src={iconSearch} width="20" alt="IMG"></img>
+        </div>
+        <input
+          type="text"
+          className="h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none"
+          placeholder="Search anything..."
+        />
+        <div className="absolute top-2 right-2">
+          <button className="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-700">
+            Search
+          </button>
         </div>
       </div>
     </div>
--- src/pages/HomePage/Index.js
+++ src/pages/HomePage/Index.js
@@ -50,33 +50,37 @@ export default function Index() {
         <Navbar />
       </div>
       <div className="ml-60">
-        <SearchBar></SearchBar>
-        {Data.map((item) => {
-          return (
-            <div class="mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
-              <div class="md:flex">
-                <div class="md:flex-shrink-0">
-                  <img
-                    class="h-48 w-ful md:w-52"
-                    src={item.picture}
-                    width="1"
-                    alt="IMG"
-                  />
-                </div>
-                <div class="p-8">
-                  <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                    {item.name}
+        <div className="fixed ml-120">
+          <SearchBar />
+        </div>
+        <div className="">
+          {Data.map((item) => {
+            return (
+              <div class="mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-1">
+                <div class="md:flex">
+                  <div class="md:flex-shrink-0">
+                    <img
+                      class="h-48 w-ful md:w-52"
+                      src={item.picture}
+                      width="1"
+                      alt="IMG"
+                    />
                   </div>
-                  <div
-                    href="#"
-                    class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-                  ></div>
-                  <div class="mt-2 text-gray-500"></div>
+                  <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                      {item.name}
+                    </div>
+                    <div
+                      href="#"
+                      class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+                    ></div>
+                    <div class="mt-2 text-gray-500"></div>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
