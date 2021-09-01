0191b69581db6cd0bc0e0ccad31931f95997ff7c
TNKKung <tomkabtokom@gmail.com>
2021-09-02 01:15:46 +0700

    update add collection and edit search bar

--- src/components/TopBarMenu/component/SearchBar.js
+++ src/components/TopBarMenu/component/SearchBar.js
@@ -1,22 +1,57 @@
-import React from "react";
-import iconSearch from "./search.png";
+import React, { useState } from "react";
+import Data from "../../Data.json";
+
 export default function SearchBar() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <div className="flex justify-center items-center min-w-full">
-      <div className="relative mt-0 shadow-xl">
-        <div className="absolute mt-5 ml-3">
-          <img src={iconSearch} width="20" alt="IMG"></img>
-        </div>
+    <div
+      className={`bg-white flex items-center h-14 w-96 ${
+        searchText.length > 2 ? "rounded-t-3xl" : "rounded-3xl"
+      } border-yellow-100 border-2 shadow-2xl`}
+    >
+      <div className="relative">
         <input
+          className="rounded-l-full w-80 py-4 px-6 text-gray-700 leading-tight focus:outline-none"
+          id="search"
           type="text"
-          className=" h-14 w-96 pl-10 pr-20 rounded-lg z-0 focus:shadow focus:outline-none "
-          placeholder="Search anything..."
+          autoComplete="off"
+          value={searchText}
+          onChange={(event) => {
+            setSearchText(event.target.value);
+          }}
+          placeholder="Search"
         />
-        <div className="absolute top-2 right-2">
-          <button className="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-blue-700">
-            Search
-          </button>
+        <div
+          className={
+            searchText.length > 2 &&
+            "absolute bg-white w-96 text-base text-black rounded-b-lg border-r-2 border-l-2 border-b-2 border-yellow-100 mr-10"
+          }
+        >
+          {Data.map((item) => {
+            return (
+              <div className="ml-5">{searchText.length > 2 && <div>{item.bookTitle}</div>}</div>
+            );
+          })}
         </div>
+      </div>
+      <div className="ml-2">
+        <button className="bg-blue-500 text-white rounded-full p-2 hover:bg-blue-400 focus:outline-none w-12 h-12 flex items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
+        </button>
       </div>
     </div>
   );
--- src/pages/AddCollectionPage/AddCollectionPage.js
+++ src/pages/AddCollectionPage/AddCollectionPage.js
@@ -31,12 +31,9 @@ export default function AddCollectionPage() {
         <div className="flex bg-white max-w-6xl w-full rounded-lg">
           <div className="flex flex-row text-black">
             <div className="flex flex-col w-screen max-w-lg items-center justify-center text-black text-xl">
-              <div className="mb-5">
-                upload images
-              </div>
               <div className="border-4 border-yellow-400 rounded-full">
                 <div class="wrapper">
-                  <div class="file-upload">
+                  <div class="file-upload flex flex-col">
                     <input type="file" />
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -52,6 +49,7 @@ export default function AddCollectionPage() {
                         d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                       />
                     </svg>
+                    <p className="text-sm text-black">upload images</p>
                   </div>
                 </div>
               </div>
@@ -60,7 +58,7 @@ export default function AddCollectionPage() {
           <div className="flex flex-col w-screen max-w-lg text-black justify-center items-center mt-7 mb-5">
             <div className="flex flex-row items-center w-full max-w-lg text-xl mb-3">
               <div className="flex flex-col justify-center w-44">
-                <div className="h-14 flex items-center">Name Of Book :</div>
+                <div className="h-14 flex items-center">Name :</div>
               </div>
               <div className="flex flex-col justify-center">
                 <div className="h-14 flex items-center w-72">
@@ -68,14 +66,14 @@ export default function AddCollectionPage() {
                     className="w-72 h-11 p-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
                     id="input-instagram"
                     type="text"
-                    placeholder={"name"}
+                    placeholder={"harry potter"}
                   />
                 </div>
               </div>
             </div>
             <div className="flex flex-row items-center w-full max-w-lg text-xl mb-3">
               <div className="flex flex-col justify-center w-44">
-                <div className="h-14 flex items-center">Name Of Book :</div>
+                <div className="h-14 flex items-center">Categories :</div>
               </div>
               <div className="flex flex-col justify-center">
                 <div className="h-14 flex items-center w-72">
@@ -101,7 +99,7 @@ export default function AddCollectionPage() {
                     className="w-72 h-11 p-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
                     id="input-instagram"
                     type="text"
-                    placeholder={"Owner"}
+                    placeholder={"nattawut"}
                   />
                 </div>
               </div>
@@ -116,7 +114,7 @@ export default function AddCollectionPage() {
                     className="w-72 h-11 p-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
                     id="input-instagram"
                     type="text"
-                    placeholder={"email"}
+                    placeholder={"tnk@gmail.com"}
                   />
                 </div>
               </div>
@@ -131,7 +129,7 @@ export default function AddCollectionPage() {
                     className="w-72 h-11 p-2 rounded-md border-2 border-gray-300 focus:border-blue-500 focus:outline-none"
                     id="input-instagram"
                     type="text"
-                    placeholder={"Tell"}
+                    placeholder={"0871772777"}
                   />
                 </div>
               </div>
